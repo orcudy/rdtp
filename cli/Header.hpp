8d024375bdf9b5bbf3c8f6d43b3cbfd784573106
Chris Orcutt <orcutt.chris@gmail.com>
2015-11-14 12:43:14 -0800

    Update project source files

---
+++ cli/Header.hpp
@@ -0,0 +1,31 @@
+//
+//  header.hpp
+//  reliable-data-protocol
+//
+//  Created by Chris Orcutt on 11/12/15.
+//  Copyright © 2015 Chris Orcutt. All rights reserved.
+//
+
+#ifndef COHeader
+#define COHeader
+
+#include <stdio.h>
+#include <string>
+
+class Header {
+  const static int dataSize = 1000;
+  
+  std::string filename;
+  bool syn;
+  bool synack;
+  int ack;
+  int seq;
+  int dataLength;
+  char data[1000];
+  
+  //constructor
+  Header();
+  
+};
+
+#endif
