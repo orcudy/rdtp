62d6789d1edde22a8684d0e4ace9561f482849de
Chris Orcutt <orcutt.chris@gmail.com>
2015-11-23 09:11:43 -0800

    Add Timer class tests

---
+++ src/Timer.cpp
@@ -0,0 +1,9 @@
+//
+//  Timer.cpp
+//  rdt
+//
+//  Created by Chris Orcutt on 11/22/15.
+//  Copyright © 2015 Chris Orcutt. All rights reserved.
+//
+
+#include "Timer.hpp"
---
+++ src/Timer.hpp
@@ -0,0 +1,14 @@
+//
+//  Timer.hpp
+//  rdt
+//
+//  Created by Chris Orcutt on 11/22/15.
+//  Copyright © 2015 Chris Orcutt. All rights reserved.
+//
+
+#ifndef Timer_hpp
+#define Timer_hpp
+
+#include <stdio.h>
+
+#endif /* Timer_hpp */
