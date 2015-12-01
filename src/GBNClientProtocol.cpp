6f9552e50ed26ae59b19660b751191d251e7e210
chloedrazen <chloedrazen@ucla.edu>
2015-11-30 18:41:17 -0800

    Update GBNClientProtocol to include functionality

--- src/GBNClientProtocol.cpp
+++ src/GBNClientProtocol.cpp
@@ -7,3 +7,88 @@
 //
 
 #include "GBNClientProtocol.hpp"
+#include "Header.hpp"
+#include "UDPCommunicator.hpp"
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+GBNClientProtocol::GBNClientProtocol(int windowSize, string ip, int port, string fname)
+{
+    communicator = UDPCommunicator(ip, port);
+    this->windowSize = windowSize;
+    //Double check this:
+    expectedAck = 0;
+    expectedSeq = 0;
+    filename = fname;
+}
+
+void GBNClientProtocol::sendSyn(std::string filename)
+{
+    Header header = Header();
+    header.syn = true;
+    header.seqNum = 0; //for now
+    strncpy(header.filename, filename.c_str(), filename.length());
+    //filename = header.filename; //wrong?
+    
+    communicator.send(header.generateMessage());
+    
+}
+
+void GBNClientProtocol::sendAck(int seqNum, int ackNum){
+    Header header = Header();
+    header.ackNum = ackNum;
+    header.seqNum = seqNum;
+    
+    communicator.send(header.generateMessage());
+}
+
+void GBNClientProtocol::receiveSynAck()
+{
+    char* message = communicator.receive();
+    Header * header = ((Header *) message);
+    
+    fileLength = header->fileSize; //Check this?!
+    
+
+    sendAck(0, 0);
+    
+    
+}
+
+//Check it's what we're expecting and if so write to file
+//Update bytesReceived
+void GBNClientProtocol::receiveData()
+{
+    char* message = communicator.receive();
+    Header * header = ((Header *) message);
+    
+    if(header->ackNum == expectedAck)
+    {
+        writeTofile(header->data);
+        expectedAck++;
+        bytesReceived += header->dataSize;
+        
+        //Not totally sure how to update expectedSeq...
+        expectedSeq++;
+    }
+    
+    //Check if end of file:
+    if(bytesReceived >= fileLength)
+    {
+        //Begin closing connection
+    }
+
+    
+}
+
+void GBNClientProtocol::writeTofile(std::string data)
+{
+    ofstream outputFile;
+    outputFile.open(filename, ios::app);
+    outputFile << data;
+    
+}
--- src/GBNClientProtocol.hpp
+++ src/GBNClientProtocol.hpp
@@ -10,17 +10,45 @@
 #define CDGBNClientProtocol
 
 #include <stdio.h>
+#include <string>
+#include "UDPCommunicator.hpp"
+
 
 
 class GBNClientProtocol {
-  //client
-  GBNClientProtocol(int windowSize, char * ip, int port);
-  
-  // RECEIVING SIDE (Client)
-  int corruptionProbability;
-  int lostProbability;
-  int expectedSeq;
-  int bytesReceived;
+public:
+    //client
+    GBNClientProtocol(int windowSize, std::string ip, int port, std::string fname);
+    
+    // RECEIVING SIDE (Client)
+    int corruptionProbability;
+    int lostProbability;
+    int expectedSeq;
+    int expectedAck;
+    int bytesReceived;
+    int fileLength; //Given by server during handshake
+    
+    
+    void sendSyn(std::string filename);
+    void receiveSynAck();
+    void sendAck(int seqNum, int ackNum);
+    
+    //Check it's what we're expecting and if so write to file
+    //Update bytesReceived
+    void receiveData();
+    
+    void writeTofile(std::string data);
+    UDPCommunicator communicator;
+    
+    //std::string filename; //temporarily public instead of private
+
+    
+private:
+    std::string filename;
+    double timerWindow;
+    int windowSize;
+    
+    
 };
 
 
